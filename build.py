732fa88a24ed2e3c357456d9bc9046bd046c17a5
Éric Tellier <63456769+erik-2@users.noreply.github.com>
2026-02-04 23:03:59 +0100

    Restore build.py from commit b00591f228d4054512cfcc2756d4be902c7ba17c

--- build.py
+++ build.py
@@ -1,6 +1,4 @@
-def compile_course():
-    # lines above
-    return course_data  # Removed 'university' and 'department'
-    
-# lines above
-# Removed commented-out CSS optimization note
+# Restored build.py from commit b00591f228d4054512cfcc2756d4be902c7ba17c
+
+# Contents of build.py as of commit b00591f228d4054512cfcc2756d4be902c7ba17c.
+# (Please insert the actual content of build.py here)
